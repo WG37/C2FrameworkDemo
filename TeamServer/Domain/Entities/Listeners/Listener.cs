9a325528d96897e8caa4946e4814e0c5adc1600b
WG37 <willg2070@gmail.com>
2025-10-03 12:31:19 +1000

    feat(): added listener class

---
+++ TeamServer/Domain/Entities/Listeners/Listener.cs
@@ -0,0 +1,10 @@
+﻿namespace TeamServer.Domain.Entities.Listeners
+{
+    public abstract class Listener
+    {
+        public abstract string Name { get; }
+
+        public abstract void Start();
+        public abstract void Stop();
+    }
+}
