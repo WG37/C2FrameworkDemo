155f9a3c90a874391a258738046990bab3706493
WG37 <willg2070@gmail.com>
2025-10-03 13:21:48 +1000

    fix(): changed entity names for clarity

---
+++ TeamServer/Domain/Entities/Listeners/HttpListenerEntity.cs
@@ -0,0 +1,36 @@
+﻿namespace TeamServer.Domain.Entities.Listeners
+{
+    public class HttpListenerEntity : ListenerEntity
+    {
+        public int BindPort { get; set; }
+        public override string Name { get; }
+
+        private CancellationTokenSource _tokenSource;
+
+        public HttpListenerEntity(string name, int bindPort)
+        {
+            Name = name;
+            BindPort = bindPort;
+        }
+
+        public override void Start()
+        {
+            _tokenSource = new CancellationTokenSource();
+
+            var builder = WebApplication.CreateBuilder();
+            builder.WebHost.UseUrls($"http://{Name}:{BindPort}");
+            builder.Services.AddControllers();
+
+            var host = builder.Build();
+
+            host.MapControllers();
+
+            host.RunAsync(_tokenSource.Token);
+        }
+
+        public override void Stop()
+        {
+            _tokenSource.Cancel();
+        }
+    }
+}
---
+++ TeamServer/Domain/Entities/Listeners/ListenerEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace TeamServer.Domain.Entities.Listeners
+{
+    public abstract class ListenerEntity
+    {
+        public abstract string Name { get; }
+
+        public abstract void Start();
+        public abstract void Stop();
+    }
+}
